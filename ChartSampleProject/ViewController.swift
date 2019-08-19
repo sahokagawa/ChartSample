3907ea58cdb7cb26e6e571e808d32735b926257b
sahokagawa <karaageyuko@gmail.com>
2019-08-19 10:27:25 +0800

    first init

---
+++ ChartSampleProject/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ChartSampleProject
+//
+//  Created by 香川紗穂 on 2019/08/19.
+//  Copyright © 2019 香川紗穂. All rights reserved.
+//
+
+import UIKit
+import Charts
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
